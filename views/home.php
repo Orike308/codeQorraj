eca8e2fb0c257a4ba708ed65e03ad5dfca5d2b81
Orike308 <aurelien02.corre@gmail.com>
2026-05-06 04:22:11 +0200

    Mise à jour de la page d'accueil : ajout de sections pour les spécialisations et caractéristiques, amélioration de la structure HTML et du style CSS.

--- views/home.php
+++ views/home.php
@@ -29,9 +29,59 @@
 
         </div>
 
-        <div id="titreAccueil">
-            <h1>Le Centre Qorraj prône une approche humaniste des soins.
-                Une équipe pluridisciplinaire impliquée, expérimentée et créative vous propose un accompagnement individualisé 7/7 jours à votre domicile.</h1>
+        <div class="texteMoyen">
+            <p>Le Centre Qorraj prône une approche humaniste des soins.
+                Une équipe pluridisciplinaire impliquée, expérimentée et créative vous propose un accompagnement individualisé 7/7 jours à votre domicile.</p>
+        </div>
+
+        <div id="accueilBt">
+            <div id="carteBtAccueil">
+
+                <div class="titreBtAccueil">
+                    <p>Nos spécialisations :</p>
+                </div>
+
+                <div class="btAccueil accueilSpecial">
+                    <p>Périnatalité</p>
+                </div>
+
+                <div class="btAccueil accueilSpecial">
+                    <p>Pédopsychiatrie</p>
+                </div>
+
+                <div class="btAccueil accueilSpecial">
+                    <p>Adulte</p>
+                </div ">
+
+                <div class="btAccueil accueilSpecial">
+                    <p>Psychogériatrie</p>
+                </div>
+
+            </div>
+
+            <div id="carteBtAccueil">
+
+                <div class="titreBtAccueil">
+                    <p>Nos caractéristiques :</p>
+                </div>
+
+                <div class="btAccueil accueilCaract">
+                    <p>Expertise</p>
+                </div>
+
+                <div class="btAccueil accueilCaract">
+                    <p>Soins personnalisés</p>
+                </div>
+
+                <div class="btAccueil accueilCaract">
+                    <p>Humanité</p>
+                </div>
+
+                <div class="btAccueil accueilCaract">
+                    <p>Rapidité</p>
+                </div>
+
+            </div>
         </div>
 
 
